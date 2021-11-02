af360ac36dc04a17a9028ae060025dd618ec1024
Marcel Frey <marcelfrey29@mailbox.org>
2021-11-02 19:40:32 +0100

    feat(): configure keycloak with terraform

---
+++ terraform_config/variables.tf
@@ -0,0 +1,35 @@
+variable "keycloak_url" {
+    description = "The URL of the Keycloak server."
+    type        = string
+    default     = "http://localhost:8080"
+}
+
+variable "keycloak_client_id" {
+    description = "The Keycloak client id ('admin-cli' if Keycloak is accessed via username and password)."
+    type        = string
+    default     = "admin-cli"
+}
+
+variable "keycloak_username" {
+    description = "The Keycloak user name of the admin account."
+    type        = string
+}
+
+variable "keycloak_password" {
+    description = "The Keycloak password of the admin account."
+    type        = string
+}
+
+variable "keycloak_redirect_frontend" {
+    description = "The URIs keycloak is allowed to redirect for the frontend application"
+    type        = list(string)
+    default     = [
+        "http://localhost:3000/*"
+    ]
+}
+
+variable "keycloak_root_url_frontend" {
+    description = "The root url is applied to all relative urls."
+    type        = string
+    default     = "http://localhost:3000"
+}

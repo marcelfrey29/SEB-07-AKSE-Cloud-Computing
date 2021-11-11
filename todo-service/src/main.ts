f92a0c32efeade907443586f05c25200d95fcd38
Marcel Frey <marcelfrey29@mailbox.org>
2021-11-11 19:21:59 +0100

    feat(): containerized the todo-service

--- todo-service/src/main.ts
+++ todo-service/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3001);
+    const configService: ConfigService = app.get(ConfigService);
+    await app.listen(configService.get<string>('SERVER_PORT') ?? 4000);
 }
+
 bootstrap();

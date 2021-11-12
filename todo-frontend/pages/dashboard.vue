0dfd25d3b53a13aa71579945cd5d7d081df4fbbd
Marcel Frey <marcelfrey29@mailbox.org>
2021-11-12 19:20:57 +0100

    fix(): run prettier

--- todo-frontend/pages/dashboard.vue
+++ todo-frontend/pages/dashboard.vue
@@ -5,8 +5,12 @@
                 <b-col class="p-0">
                     Dashboard
                     <div>
-                        <div>Response: <b>{{ serviceResponse }}</b></div>
-                        <b-button variant="primary" @click="getServiceResponse">Check Backend Connection</b-button>
+                        <div>
+                            Response: <b>{{ serviceResponse }}</b>
+                        </div>
+                        <b-button variant="primary" @click="getServiceResponse"
+                            >Check Backend Connection</b-button
+                        >
                     </div>
                 </b-col>
             </b-row>
@@ -20,14 +24,14 @@ import Component from 'vue-class-component'
 
 @Component({})
 export default class Dashboard extends Vue {
-    private serviceResponse = " "
+    private serviceResponse = ' '
 
     async getServiceResponse(): Promise<void> {
-        let response = ""
+        let response = ''
         try {
             response = await this.$axios.$get('http://localhost:4000/priv')
         } catch (err) {
-            response = "ERROR: " + err
+            response = 'ERROR: ' + err
         }
         this.serviceResponse = response
     }

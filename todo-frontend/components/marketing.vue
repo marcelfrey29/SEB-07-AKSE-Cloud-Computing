56659aae7ad36cf2e172e690dc22b6f2c94dd99d
Marcel Frey <marcelfrey29@mailbox.org>
2021-11-19 21:35:55 +0100

    feat(): general ui/ux improvements including a new marketing page

--- todo-frontend/components/marketing.vue
+++ todo-frontend/components/marketing.vue
@@ -1,33 +1,54 @@
 <template>
     <b-container>
-        <b-row class="mt-4">
+        <b-row class="mt-5">
             <b-col>
-                <h2>Get Things Done!</h2>
-                <h3>With the Todo App</h3>
-                <b-button variant="warning" @click="login"
-                    >Start Getting Things Done!</b-button
-                >
+                <h2>Get Things Done...</h2>
+                <h2 style="padding-left: 31px">
+                    ...with the <span class="text-primary">Todo App</span>!
+                </h2>
+                <b-button class="mt-3" variant="warning" @click="login"
+                    >Login or Register
+                </b-button>
             </b-col>
         </b-row>
 
-        <b-row cols="1" cols-md="2" cols-lg="3">
+        <b-row class="mt-1">
             <b-col>
-                <b-card class="mt-5">Feature #1</b-card>
-            </b-col>
-            <b-col>
-                <b-card class="mt-5">Feature #2</b-card>
-            </b-col>
-            <b-col>
-                <b-card class="mt-5">Feature #3</b-card>
+                <b-card-group deck>
+                    <b-card
+                        class="mt-5"
+                        title="Lists"
+                        sub-title="Organize your Todos with Lists."
+                        img-src="/lists.png"
+                        img-top
+                    ></b-card>
+
+                    <b-card
+                        class="mt-5"
+                        title="Powerful organization"
+                        sub-title="Add a description, a due date and tags to your todos."
+                        img-src="/todos.png"
+                        img-top
+                    ></b-card>
+
+                    <b-card
+                        class="mt-5"
+                        title="Tags"
+                        sub-title="Tag your todos."
+                        img-src="/tags.png"
+                        img-top
+                    ></b-card>
+                </b-card-group>
             </b-col>
         </b-row>
 
-        <b-row class="mt-4">
+        <b-row class="mt-5 mb-4">
             <b-col>
-                <h2>Start getting productive</h2>
+                <h4>Sounds good?</h4>
+                <h4>Then get started now!</h4>
                 <b-button variant="warning" @click="login"
-                    >Sign in or Register</b-button
-                >
+                    >Login or Register
+                </b-button>
             </b-col>
         </b-row>
     </b-container>
--- todo-frontend/nuxt.config.js
+++ todo-frontend/nuxt.config.js
@@ -26,6 +26,12 @@ export default {
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [],
 
+    // Loading
+    loading: {
+        color: 'white',
+        height: '3px',
+    },
+
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
     plugins: [],
 

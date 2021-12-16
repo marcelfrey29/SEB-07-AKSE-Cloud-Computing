b49e7a1b302668856dadd267eb13e9872f203182
Marcel Frey <marcelfrey29@mailbox.org>
2021-12-16 18:39:49 +0100

    feat(): upload frontend application to s3 bucket

---
+++ terraform/workspaces/aws/locals.tf
@@ -0,0 +1,12 @@
+locals {
+    mime_types = {
+        ".html" : "text/html",
+        ".css" : "text/css",
+        ".js" : "text/javascript",
+        ".ico" : "image/vnd.microsoft.icon",
+        ".jpg" : "image/jpeg",
+        ".jpeg" : "image/jpeg",
+        ".json" : "application/json"
+        "DEFAULT_VALUE" : "text/plain"
+    }
+}
--- terraform/workspaces/aws/s3-webapp.tf
+++ terraform/workspaces/aws/s3-webapp.tf
@@ -12,3 +12,17 @@ resource "aws_s3_bucket" "todo-frontend" {
 
     tags = var.aws_tags
 }
+
+resource "aws_s3_bucket_object" "files" {
+    for_each = fileset("${path.module}/../../../todo-frontend/dist/", "**")
+    bucket   = aws_s3_bucket.todo-frontend.bucket
+    key      = each.value
+    source   = "${path.module}/../../../todo-frontend/dist/${each.value}"
+    etag     = filemd5("${path.module}/../../../todo-frontend/dist/${each.value}")
+
+    # Fix MIME-Type
+    # Otherwise static website hosting does not work
+    content_type = lookup(local.mime_types, try(regex("\\.[^.]+$", each.value), "DEFAULT_VALUE"), "text/plain")
+
+    tags = var.aws_tags
+}

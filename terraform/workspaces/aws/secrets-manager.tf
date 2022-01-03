e1e45496e06fa1ad56c32d0ecb4ff3ac5e9531de
Marcel Frey <marcelfrey29@mailbox.org>
2022-01-03 11:48:36 +0100

    feat(): add SecretsManager for RDS Credentials
    When we launch containers via ECS, we can pass secrets via the secret field and not via environment variables. This results in better security, because credentials are not stored in plain text. In ECS we can pass an ARN of a secret.

---
+++ terraform/workspaces/aws/secrets-manager.tf
@@ -0,0 +1,16 @@
+resource "aws_secretsmanager_secret" "keycloak_rds_db_user_password" {
+    name                    = "RDS-Postgres-Password"
+    description             = "The password to access the RDS Postgres Instance (Required for Keycloak)"
+    recovery_window_in_days = 0 # Enable force deletion
+    tags                    = var.aws_tags
+}
+
+resource "aws_secretsmanager_secret_version" "keycloak_rds_db_user_password" {
+    secret_id     = aws_secretsmanager_secret.keycloak_rds_db_user_password.id
+    secret_string = var.keycloak_db_password
+}
+
+resource "aws_secretsmanager_secret_policy" "keycloak_rds_db_user_password" {
+    secret_arn = aws_secretsmanager_secret.keycloak_rds_db_user_password.arn
+    policy     = file("${path.module}/policies/secrets-manager-secret-access.json")
+}
